1551be187208dd27afaa041e738469028a7d9223
ilker can <ilker@pineal.com.tr>
2022-05-17 20:53:52 +0300

    Initial

--- src/main/java/com/ican/initial/demo/UsersRepository.java
+++ src/main/java/com/ican/initial/demo/UsersRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
+// Consider changing to JpaRepository
 
 public interface UsersRepository extends CrudRepository<Users, Integer> {
 
