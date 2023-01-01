6d88c35fbf871aecf1e649d6687a9584e2d44832
ilker can <ilker@pineal.com.tr>
2023-01-01 20:12:40 +0300

    Done

--- src/main/java/com/ican/initial/demo/DemoApplication.java
+++ src/main/java/com/ican/initial/demo/DemoApplication.java
@@ -8,6 +8,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(info = @Info(title = "Rest API Documentation", version = "1.0", description = "Spring Boot Rest API by ICAN"))
 
+/*
+ * 
+ * SWAGGER URL : http://localhost:8080/swagger-ui/index.html
+ */
+
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/ican/initial/demo/Users/UsersController.java
+++ src/main/java/com/ican/initial/demo/Users/UsersController.java
@@ -3,6 +3,8 @@ package com.ican.initial.demo.Users;
 import com.github.dozermapper.core.Mapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,14 +30,16 @@ public class UsersController {
 
     @Operation(summary = "Adds a new user to the database", description = "All the fields are required.")
     @PostMapping(path = "/add2") // Map ONLY POST Requests
-    public @ResponseBody Users addNewUserWithRbUserClass(@RequestBody RBUsers rbUsers) {
+    public ResponseEntity<Users> addNewUserWithRbUserClass(@RequestBody RBUsers rbUsers) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
         // rbUsers.setemail("email");
         Users n = new Users();
         mapper.map(rbUsers, n);
-        return usersService.Save(n);
+
+        return ResponseEntity.status(HttpStatus.OK).body(usersService.Save(n));
+        // return ;
 
         // return "Saved";
     }
